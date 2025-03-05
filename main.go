5a555665003f9b67a3c9bf679c619be4f70c2816
Pasit Khumsena <webbalaka@gmail.com>
2025-03-06 00:45:38 +0700

    feat(display): add display animation

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	prefix "Takluz_TTS/audio/prefix"
-	audio "Takluz_TTS/audio/sound"
+	"Takluz_TTS/audio/prefix"
+	fetchsound "Takluz_TTS/audio/sound"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"text/template"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 
@@ -13,6 +17,63 @@ import (
 	"github.com/andreykaipov/goobs/api/requests/inputs"
 	"github.com/andreykaipov/goobs/api/requests/mediainputs"
 )
+
+type Message struct {
+	UserName string `json:"userName"`
+	Message  string `json:"message"`
+}
+
+func playAnimation(client *goobs.Client, inputName, htmlDirectory string, userName string, message string) error {
+	// if len(message) >= 50 {
+	// 	message = message[:50] + "..."
+	// }
+	funcMap := template.FuncMap{
+		"split": strings.Split,
+	}
+
+	tmpl := template.Must(template.New("index.html").Funcs(funcMap).ParseFiles(htmlDirectory + "/index.html"))
+
+	data := struct {
+		UserName string
+		Message  string
+	}{
+		UserName: userName,
+		Message:  message,
+	}
+
+	f, err := os.Create(htmlDirectory + "/print.html") //Create file named output.html
+	if err != nil {
+		log.Fatal("Error creating file:", err)
+	}
+	defer f.Close() // Ensure the file is closed, even if there's an error.
+
+	err = tmpl.Execute(f, data) //write to a file
+	if err != nil {
+		log.Fatal("Error writing to file:", err)
+	}
+
+	_, err = client.Inputs.SetInputSettings(inputs.NewSetInputSettingsParams().WithInputName(inputName).WithInputSettings(map[string]interface{}{
+		"url": "file://" + htmlDirectory + "/print.html",
+	}))
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Inputs.SetInputSettings(inputs.NewSetInputSettingsParams().WithInputName(inputName).WithInputSettings(map[string]interface{}{
+		"refresh": true,
+	}))
+
+	time.AfterFunc(8*time.Second, func() {
+		_, err := client.Inputs.SetInputSettings(inputs.NewSetInputSettingsParams().WithInputName(inputName).WithInputSettings(map[string]interface{}{
+			"url": "",
+		}))
+		if err != nil {
+			log.Println("Failed to turn off animation:", err)
+		}
+	})
+
+	return err
+}
 
 func playSound(client *goobs.Client, inputName, filePath string) error {
 	// _, err := client.MediaInputs.SetMediaInputCursor(mediainputs.NewSetMediaInputCursorParams().WithInputName(inputName).WithMediaCursor(0))
@@ -33,10 +94,8 @@ func playSound(client *goobs.Client, inputName, filePath string) error {
 
 func main() {
 
-	// prefix.ConcatAudio([]string{"sample-3s.mp3", "speech.mp3"}, "xdd.mp3")
-	fmt.Println("done")
 	//เปลี่ยน Key
-	OPEN_API_KEY := os.Getenv("OPEN_API_KEY")
+	OPEN_API_KEY := os.Getenv("OPENAI_API_KEY")
 	OBS_KEY := os.Getenv("OBS_KEY")
 	limitToken := 200
 	//
@@ -52,11 +111,16 @@ func main() {
 	}
 	defer client.Disconnect()
 
+	htmlPath := filepath.Join(currentDir, "templates")
+	htmlPath = filepath.ToSlash(htmlPath)
+	err = playAnimation(client, "Browser", htmlPath, "WebBacillus", "Test Message")
+	if err != nil {
+		log.Println(err.Error())
+	}
+
 	app := fiber.New()
 	app.Post("/", func(c *fiber.Ctx) error {
-		var message struct {
-			Message string `json:"message"`
-		}
+		var message Message
 		if err := c.BodyParser(&message); err != nil {
 			return c.Status(400).JSON(fiber.Map{"error": err.Error()})
 		}
@@ -65,7 +129,7 @@ func main() {
 			message.Message = message.Message[:limitToken]
 		}
 
-		audio.GetSound(message.Message, OPEN_API_KEY, "speech.mp3")
+		fetchsound.GetSound(message.Message, OPEN_API_KEY, "speech.mp3")
 		prefix.ConcatAudio([]string{"sample-3s.mp3", "speech.mp3"}, "output.mp3")
 
 		outPath := filepath.Join(currentDir, "output.mp3")
@@ -74,7 +138,14 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(len(message.Message), "characters:", message.Message)
+
+		htmlPath := filepath.Join(currentDir, "templates")
+		htmlPath = filepath.ToSlash(htmlPath)
+		err = playAnimation(client, "Browser", htmlPath, message.UserName, message.Message)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		fmt.Println(message.UserName, "used", len(message.Message), "characters", message.Message)
 		return c.Status(200).SendString(message.Message)
 
 	})
